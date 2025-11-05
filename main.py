dc9475055ffe5b74101989ccfe843ea3085d0819
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-11-05 11:47:18 -0700

    started

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+import math
+from pygame import mixer
+
+pygame.init()
+
+screen = pygame.display.set_mode((1600,1200))
+pygame.display.set_caption("UT-Fight")
+pygame_icon = pygame.image.load(r'resources\soul.jpeg')
+pygame.display.set_icon(pygame_icon)
+
+
+def main():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        #Loops
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+        pygame.display.flip
+
+main()

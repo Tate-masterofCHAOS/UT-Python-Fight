7fb6964ae4ccd23435490b334fba0d615bf343fe
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-11-06 16:46:27 +0000

    Worked on a few thing, planning on testing them in a few hours as well as added a few resources namely the buttons

--- classes.py
+++ classes.py
@@ -39,6 +39,14 @@ class Attack:
     def perform_attack(self):
         pass
 
+class Attack_type_A(Attack):
+    def __init__(self, length):
+        super().__init__(length)
+
+    def perform_attack(self):
+        pass
+
+
 class Enemy:
     def __init__(self, health):
         self.health = health
@@ -47,19 +55,26 @@ class Enemy:
         pass
 
 class Player:
-    def __init__(self, change=0):
+    def __init__(self, x, y, change=0):
         img = pygame.image.load(r'resources/soul.png')
         self.img = pygame.transform.scale(img, (64, 64))  # Scale the image to fit the screen 
-        self.x = 800-32
-        self.y = 600-32
-        self.change = change
-        self.score = 0
+        self.x = x
+        self.y = y
+        self.speed = 5
+
+    def update(self, keys, boundary_rect):
+            if keys[pygame.K_LEFT]:
+                self.rect.x -= self.speed
+            if keys[pygame.K_RIGHT]:
+                self.rect.x += self.speed
+            if keys[pygame.K_UP]:
+                self.rect.y -= self.speed
+            if keys[pygame.K_DOWN]:
+                self.rect.y += self.speed
+
+            self.rect.clamp_ip(boundary_rect)
 
     def player_set(self):
         screen.blit(self.img, (self.x, self.y))
-
-    def move(self):
-        self.x += self.change
-        self.y += self.change
 
 #Take button class from space invaders
--- main.py
+++ main.py
@@ -11,7 +11,8 @@ pygame.display.set_caption("UT-Fight")
 pygame_icon = pygame.image.load(r'resources\soul.png')
 pygame.display.set_icon(pygame_icon)
 
-player = Player()
+player = Player(800-32, 600-32)
+battle_box = pygame.Rect(300, 200, 1000, 800)
 
 def main():
     running = True
@@ -20,18 +21,29 @@ def main():
         #Loops
         for event in pygame.event.get():
             keys = pygame.key.get_pressed()
-            keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if keys[pygame.K_LEFT]:
-                    player.change = -0.3
-                if keys[pygame.K_RIGHT]:
-                    player.change = 0.3
 
-        player.player_set
+        player.player_set()
+        screen.blit(pygame.Surface(battle_box.size), battle_box.topleft)
+
+        
+        player.update(keys, battle_box.get_rect())
 
         
         pygame.display.flip
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+#https://www.google.com/search?q=how+to+make+a+rectangle+with+an+object+inside+that+cant+get+out+the+rectangle+pygame&rlz=1CAJIKU_enUS1187&oq=how+to+make+a+rectangle+with+an+object+inside+that+cant+get+out+the+rectangle+pygame&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTIwODM3ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8&safe=active&ssui=on
+#Code to clamp soul inside recangle

4ea37c7dc356c85b86c760f8eed52dbf7efc8907
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-11-06 08:55:37 -0700

    worked a litte bit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pygame.display.set_caption("UT-Fight")
 pygame_icon = pygame.image.load(r'resources\soul.png')
 pygame.display.set_icon(pygame_icon)
 
+player = Player()
 
 def main():
     running = True
@@ -19,8 +20,16 @@ def main():
         #Loops
         for event in pygame.event.get():
             keys = pygame.key.get_pressed()
+            keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if keys[pygame.K_LEFT]:
+                    player.change = -0.3
+                if keys[pygame.K_RIGHT]:
+                    player.change = 0.3
+
+        player.player_set
 
         
         pygame.display.flip
